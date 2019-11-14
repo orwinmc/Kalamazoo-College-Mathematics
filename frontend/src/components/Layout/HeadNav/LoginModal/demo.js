cb86a049c0ffe0424a8d3cc02ab270c3e4e204e5
Dahwi Kim <ekgnlqlem@gmail.com>
2019-11-13 22:17:35 -0500

    add a new Modal component

---
+++ frontend/src/components/Layout/HeadNav/LoginModal/demo.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {Modal, Button, Row, Col, Form} from 'react-bootstrap';
+
+export default class Demo extends Component{
+    constructor(props){
+        super(props);
+
+    }
+
+    render(){
+        return(
+            <Modal
+            {...this.props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="contained-modal-title-vcenter">
+                Please type in your ID and password
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div className="Container">
+
+                </div>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant ="danger" onClick={this.props.onHide}>Close</Button>
+            </Modal.Footer>
+          </Modal>
+        );
+    }
+}
