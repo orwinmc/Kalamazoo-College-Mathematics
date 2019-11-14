73a028b4214cd4a0c2e00a1f15ca20417fe126b8
Dahwi Kim <ekgnlqlem@gmail.com>
2019-11-13 22:08:58 -0500

    create a login page

--- frontend/src/components/Layout/HeadNav/HeadNav.js
+++ frontend/src/components/Layout/HeadNav/HeadNav.js
@@ -2,22 +2,39 @@ import React, {Component} from 'react';
 
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
 import './HeadNav.css'
+//import LoginModal from './LoginModal/LoginModal';
+import Demo from './LoginModal/demo';
+import {Button, ButtonToolbar} from 'react-bootstrap';
+import {Redirect } from 'react-router-dom';
 
 class HeadNav extends Component {
-  /*constructor(props) {
+  constructor(props) {
     super(props);
-
-    this.state = {}
-  }*/
+  }
 
   render() {
+
+    let addModalClose = () => this.setState({ addModalShow: false })
+
     return (
       <Navbar fixed variant="dark" className="HeadNav">
         <Navbar.Brand href="/">Kalamazoo College Mathematics</Navbar.Brand>
         <Nav className="ml-auto">
-          <Nav.Link href="/home">Login</Nav.Link>
+          {/* <Nav.Link href="/home">Login</Nav.Link> */}
+          <ButtonToolbar>
+            <Button 
+            variant ='primary' 
+            onClink={()=> <Redirect to='/demo' />}
+            >Log In
+            </Button>
+
+            <Demo 
+              show={this.state.addModalShow}
+              onHide={addModalClose}
+            />
+
+          </ButtonToolbar>
         </Nav>
       </Navbar>
     );
--- frontend/src/components/Layout/HeadNav/LoginModal/LoginModal.js
+++ frontend/src/components/Layout/HeadNav/LoginModal/LoginModal.js
@@ -25,15 +25,15 @@ class LoginModal extends Component {
       <div class="LoginModal">
         <Modal show={show} onHide={handleClose}>
           <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
+            <Modal.Title>Log In</Modal.Title>
           </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Body>ID, PS</Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
               Close
             </Button>
             <Button variant="primary" onClick={handleClose}>
-              Save Changes
+              Log In
             </Button>
           </Modal.Footer>
         </Modal>
